1e3f13b93b0663dde8453221ff20b1c10a6504df
coding-salmon <ejzus88@naver.com>
2024-02-20 10:30:07 +0900

    ?!

--- _01_FirstJavaProjet/src/first01/hello01/HelloWorld.java
+++ _01_FirstJavaProjet/src/first01/hello01/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 		System.out.println("헬로우월드");
 		System.out.println("헬로우자바");
 		System.out.println("헬로우에브리원");
+		System.out.println("헬로우이클립스");
+		System.out.println("헬로우오류");
+		System.out.println("헬로우디버그");
+		
 	}
 
 }
