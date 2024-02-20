df82a6b6ce456c3ab8d87c29a1294de1a9beff50
coding-salmon <ejzus88@naver.com>
2024-02-20 10:33:48 +0900

    메인

--- _01_FirstJavaProjet/src/first01/hello01/HelloWorld.java
+++ _01_FirstJavaProjet/src/first01/hello01/HelloWorld.java
@@ -1,14 +1,17 @@
 package first01.hello01;
 
 public class HelloWorld {
-	private void mian() {
+	public static void main(String[] args) {
+
 		System.out.println("헬로우월드");
 		System.out.println("헬로우자바");
 		System.out.println("헬로우에브리원");
 		System.out.println("헬로우이클립스");
 		System.out.println("헬로우오류");
 		System.out.println("헬로우디버그");
+		System.out.println("헬로 웹");
+	} 
 		
-	}
+	
 
 }
